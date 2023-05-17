d5be7d11948b1f6496d0fd20f7ccf5bb4da8d586
itsEngRaghad <132268219+itsEngRaghad@users.noreply.github.com>
2023-05-17 07:11:45 +0300

    Add files via upload

---
+++ SpringHW13Taks/src/main/java/com/example/springhw13taks/Model/Task.java
@@ -0,0 +1,10 @@
+package com.example.springhw13taks.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Task {
+    private String id , title , description , status;
+}
